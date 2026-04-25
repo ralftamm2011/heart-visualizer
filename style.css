9c35082c8a796932b204a8db37702f78e0d00c35
ralftamm2011 <ralf.tamm2011@gmail.com>
2026-04-25 20:40:42 +0300

    Add files via upload

---
+++ style.css
@@ -0,0 +1,21 @@
+body {
+  margin: 0;
+  overflow: hidden;
+  background: radial-gradient(circle at center, #120010, #000);
+}
+
+canvas {
+  display: block;
+}
+
+#ui {
+  position: absolute;
+  top: 15px;
+  left: 15px;
+  z-index: 10;
+  opacity: 0.8;
+}
+
+#ui:hover {
+  opacity: 1;
+}
